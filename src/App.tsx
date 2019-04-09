d35eaa659f5b8873d592d313168cdf42a2372084
Pedro Costa <pcosta@techona.com>
2019-04-09 14:41:12 +0200

    Starting to route components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'bootstrap/scss/bootstrap.scss';
 
 import api from './api'
@@ -10,7 +11,12 @@ class App extends Component {
   render() {
     const userService = new UserService(api);
     return (
-      <UserList service={userService} />
+      <Router>
+        <div>
+          <Route path="/users" render={() =><UserList service={userService} />} />
+        </div>
+      </Router>
+      
     );
   }
 }
