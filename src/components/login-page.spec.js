90ba5303647840b0d50bda00c003940a265678aa
Pedro Costa <pcosta@techona.com>
2019-04-10 14:27:35 +0200

    Adding test to login page

--- src/components/login-page.spec.js
+++ src/components/login-page.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {mount, shallow} from 'enzyme'
+import sinon from 'sinon'
 
 import LoginPage from './login-page'
 
@@ -13,7 +14,7 @@ describe(`<LoginPage />`, () => {
   });
 
   it(`.generateToken()`, () => {
-    const component = mount(<LoginPage />);
+    const component = mount(<LoginPage redirect={sinon.fake()}/>);
     component.setState({name: `test`, email: `test@test.com`})
     const token = component.instance().generateToken(`teste`);
 
@@ -21,7 +22,7 @@ describe(`<LoginPage />`, () => {
   });
 
   it(`.handleNameChange()`, () => {
-    const wrapper = mount(<LoginPage />);
+    const wrapper = mount(<LoginPage redirect={sinon.fake()}/>);
     expect(wrapper.state().name).toEqual(``);
 
     wrapper.instance().handleNameChange({currentTarget: {value: `bororoska`}})
@@ -29,10 +30,20 @@ describe(`<LoginPage />`, () => {
   });
 
   it(`.handleEmailChange()`, () => {
-    const wrapper = mount(<LoginPage />);
+    const wrapper = mount(<LoginPage redirect={sinon.fake()}/>);
     expect(wrapper.state().email).toEqual(``);
 
     wrapper.instance().handleEmailChange({currentTarget: {value: `bororoska@test.com`}})
     expect(wrapper.state().email).toEqual(`bororoska@test.com`);
+  });
+
+  it(`.redirect()`, () => {
+    expect.assertions(1);
+    var callback = sinon.spy();
+    const wrapper = mount(<LoginPage redirect={callback}/>);
+    wrapper.setState({name: `test`, email: `test@test.com`})
+
+    wrapper.instance().redirect();
+    expect(callback.called).toBeTruthy();
   });
 });
