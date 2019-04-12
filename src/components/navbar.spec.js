6a7788b8dd571d8d2a0acb1887d0f96af6c9aac8
Pedro Costa <pedrocelsonunes@gmail.com>
2019-04-12 16:41:13 +0200

    Improving the navbar, adding private items and icons (#13)

--- src/components/navbar.spec.js
+++ src/components/navbar.spec.js
@@ -4,16 +4,24 @@ import sinon from 'sinon'
 
 import NavBar from './navbar'
 
+const context = { router: { isActive: (a, b) => true } };
+
 describe(`<NavBar />`, () => {
-  it(`Should render without errors`, () => {
-    const component = shallow(<NavBar redirect={sinon.fake()} />);
+  it(`Should render without errors for authenticated users`, () => {
+    const component = shallow(<NavBar authenticated={true} />, {context}, {context});
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it(`Should render without errors for unauthenticated users`, () => {
+    const component = shallow(<NavBar authenticated={false} />, {context});
 
     expect(component).toMatchSnapshot();
   });
 
   it(`.getTitle() - Should return the right title based on the document location`, () => {
     expect.assertions(2);
-    const component = mount(<NavBar redirect={sinon.fake()} />);;
+    const component = mount(<NavBar authenticated={true} />, {context});;
 
     expect(component.instance().getTitle(`/users`)).toEqual(`Users`)
     expect(component.instance().getTitle(`/tasks`)).toEqual(`Tasks`)
@@ -21,17 +29,34 @@ describe(`<NavBar />`, () => {
 
   it(`.getTitle() - Should return an empty string for unknow locations`, () => {
     expect.assertions(1);
-    const component = mount(<NavBar redirect={sinon.fake()} />);;
+    const component = mount(<NavBar authenticated={true} />, {context});;
 
     expect(component.instance().getTitle(`/gerere`)).toEqual(``)
   });
 
   it(`.toggleDrawer() - Should set the right parameter for the drawer menu`, () => {
-    const component = mount(<NavBar redirect={sinon.fake()} />);;
+    const component = shallow(<NavBar authenticated={true} />, {context});
     expect(component.state().drawerOpened).toBeFalsy();
     component.instance().toggleDrawer(true)();
     expect(component.state().drawerOpened).toBeTruthy();
     component.instance().toggleDrawer(false)();
     expect(component.state().drawerOpened).toBeFalsy();
+  });
+
+  it(`.getMenuItems() - Should return menu items according to the user authentication`, () => {
+    expect.assertions(8);
+    const component1 = mount(<NavBar authenticated={true} />, {context});
+    const authenticatedItems = component1.instance().getMenuItems();
+    expect(authenticatedItems.length).toEqual(2);
+    expect(authenticatedItems[0].title).toEqual(`Users`);
+    expect(authenticatedItems[0].path).toEqual(`/users`);
+    expect(authenticatedItems[1].title).toEqual(`Tasks`);
+    expect(authenticatedItems[1].path).toEqual(`/tasks`);
+
+    const component2 = mount(<NavBar authenticated={false} />, {context});
+    const unauthenticatedItems = component2.instance().getMenuItems();
+    expect(unauthenticatedItems.length).toEqual(1);
+    expect(unauthenticatedItems[0].title).toEqual(`Login`);
+    expect(unauthenticatedItems[0].path).toEqual(`/login`);
   });
 });
