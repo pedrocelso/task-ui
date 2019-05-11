b13876361cb69a2191980dd5723ccd2ff1a9a66b
Pedro Costa <pedrocelsonunes@gmail.com>
2019-05-11 13:38:12 +0200

    sorting incidents by status - Pending will appear first on the list

--- src/components/incident/incident-list.spec.js
+++ src/components/incident/incident-list.spec.js
@@ -7,7 +7,7 @@ import {IncidentList} from './incident-list'
 describe(`<IncidentList />`, () => {
   it(`Should render the list`, () => {
     const incidentList = [
-      { id: 10, name: `test`, description: `test@test.com`, creationTime: 1556924666404, updateTime: 1556924566404 },
+      { id: 10, name: `test`, description: `test@test.com`, creationTime: 1556924666404, updateTime: 1556924566404, status: 0 },
       { id: 11, name: `test1`, description: `test1@test.com`, creationTime: 1556924666404, updateTime: 0, status: 1 },
     ]
 
