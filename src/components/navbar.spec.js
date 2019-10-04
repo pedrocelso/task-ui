09c4fb262569f5747d5139759242770cc7fc0078
Pedro Costa <pedrocelsonunes@gmail.com>
2019-10-04 21:06:39 +0200

    Adding location to prop list on navbar

--- src/components/navbar.spec.js
+++ src/components/navbar.spec.js
@@ -18,20 +18,20 @@ const publicItems = [
 
 describe(`<NavBar />`, () => {
   it(`Should render without errors for authenticated users`, () => {
-    const component = shallow(<NavBar items={privateItems} />, { context }, { context });
+    const component = shallow(<NavBar location="/users" items={privateItems} />, { context }, { context });
 
     expect(component).toMatchSnapshot();
   });
 
   it(`Should render without errors for unauthenticated users`, () => {
-    const component = shallow(<NavBar items={publicItems} />, { context });
+    const component = shallow(<NavBar location="/users" items={publicItems} />, { context });
 
     expect(component).toMatchSnapshot();
   });
 
   it(`.getTitle() - Should return the right title based on the document location`, () => {
     expect.assertions(2);
-    const component = mount(<NavBar items={privateItems} />, { context });;
+    const component = mount(<NavBar location="/users" items={privateItems} />, { context });;
 
     expect(component.instance().getTitle(`/users`)).toEqual(`Users`)
     expect(component.instance().getTitle(`/tasks`)).toEqual(`Tasks`)
@@ -39,13 +39,13 @@ describe(`<NavBar />`, () => {
 
   it(`.getTitle() - Should return an empty string for unknow locations`, () => {
     expect.assertions(1);
-    const component = mount(<NavBar items={privateItems} />, { context });;
+    const component = mount(<NavBar location="/users" items={privateItems} />, { context });;
 
     expect(component.instance().getTitle(`/gerere`)).toEqual(``)
   });
 
   it(`.toggleDrawer() - Should set the right parameter for the drawer menu`, () => {
-    const component = shallow(<NavBar items={publicItems} />, { context });
+    const component = shallow(<NavBar location="/users" items={publicItems} />, { context });
     expect(component.state().drawerOpened).toBeFalsy();
     component.instance().toggleDrawer(true)();
     expect(component.state().drawerOpened).toBeTruthy();
@@ -53,4 +53,10 @@ describe(`<NavBar />`, () => {
     expect(component.state().drawerOpened).toBeFalsy();
   });
 
+  it(`.render() - Should render the create button on tasks`, () => {
+    const tskComponent = shallow(<NavBar location="/tasks" items={privateItems} />, { context });
+    expect(tskComponent).toMatchSnapshot();
+    const usrComponent = shallow(<NavBar location="/users" items={publicItems} />, { context });
+    expect(usrComponent).toMatchSnapshot();
+  });
 });
